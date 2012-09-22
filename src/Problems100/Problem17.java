e0aab37f7725f48b651217a055dffc6e1f03bac3
Volodymyr Shulgin <vladimir.shulgin@gmail.com>
2012-09-22 23:02:35 +0200

    Revert "Скоро доделаю Серенький!"
    This reverts commit e4aa5671810a9213fc702bf147473adefa35a79d.

--- src/Problems100/Problem17.java
+++ src/Problems100/Problem17.java
@@ -7,33 +7,7 @@ public class Problem17 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-            String[] smallNumbers = new String[]{"","one","two","three"
-            + "four","five","six","seven","eight","nine","ten","eleven","twelve"
-                    + "thirteen","fourteen","fifteen","sixteen","seventeen"
-            + "eightteen","nineteen","twenty"};
-            String[] bigNumbers=new String[]{"thirty","fourty","fifty"
-                    + "sixty","seventy","eighty","ninety","hundred"};
-            String word="";
-            int Summ=0;
-            for(int i=1;i<=1000;i++)
-            {
-                if(i!=1000) 
-                {
-                    if(i<21)
-                    {
-                        Summ+=smallNumbers[i].length();
-                    }
-                    if(i>21&&i<100)
-                    {
-                        
-                    }
-                    
-                }
-                else
-                {
-                    Summ+=11;
-                }
-            }
+
 	}
 
 }
