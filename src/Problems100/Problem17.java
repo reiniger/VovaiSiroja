b222b72a0245d1f61dcf85148d67083e5828bc8d
Volodymyr Shulgin <vladimir.shulgin@gmail.com>
2012-09-22 22:18:59 +0200

    Сделано!

--- src/Problems100/Problem17.java
+++ src/Problems100/Problem17.java
@@ -5,35 +5,73 @@ public class Problem17 {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-            String[] smallNumbers = new String[]{"","one","two","three"
-            + "four","five","six","seven","eight","nine","ten","eleven","twelve"
-                    + "thirteen","fourteen","fifteen","sixteen","seventeen"
-            + "eightteen","nineteen","twenty"};
-            String[] bigNumbers=new String[]{"thirty","fourty","fifty"
-                    + "sixty","seventy","eighty","ninety","hundred"};
-            String word=""; 
-            int Summ=0;
-            for(int i=1;i<=1000;i++)
+	public static void main(String[] args) 
+        {
+            String[] smallNumbers = new String[]{"","one","two","three",
+             "four","five","six","seven","eight","nine","ten","eleven","twelve",
+                    "thirteen","fourteen","fifteen","sixteen","seventeen"
+            , "eighteen","nineteen"};
+            String[] bigNumbers=new String[]{"hundred","","twenty","thirty",
+                "forty","fifty", "sixty","seventy","eighty","ninety",};
+            int Summ=11;
+            int temp=0;
+            for(int i=1;i<1000;i++)
             {
-                if(i!=1000) 
+                if(i<20)
                 {
-                    if(i<21)
-                    {
-                        Summ+=smallNumbers[i].length();
-                    }
-                    if(i>21&&i<100)
-                    {
-                        
-                    }
-                    
+                    Summ+=smallNumbers[i].length();
                 }
-                else
+                if(i>=20&&i<100)
                 {
-                    Summ+=11;
+                  if(i%10!=0)
+                  {
+                      temp=i/10;
+                      Summ+=bigNumbers[temp].length();
+                      temp=i%10;
+                      Summ+=smallNumbers[temp].length();
+                  }
+                  else
+                  {
+                      Summ+=bigNumbers[i/10].length();
+                  }
                 }
+                if(i>=100)
+                {
+                    if(i%100==0)
+                    {
+                        Summ+=smallNumbers[i/100].length()+
+                                bigNumbers[0].length();                   
+                    }
+                    else
+                    {
+                        Summ+=3;
+                        temp=i/100;
+                        Summ+=smallNumbers[temp].length()+bigNumbers[0].length();
+                        temp=i%100;
+                        if(temp<20)
+                        {
+                            Summ+=smallNumbers[temp].length();
+                        }
+                         if(temp>=20)
+                        {
+                          if(temp%10!=0)
+                          {
+                              
+                              int temp2=temp/10;
+                              Summ+=bigNumbers[temp2].length();
+                              temp2=temp%10;
+                              Summ+=smallNumbers[temp2].length();
+                          }
+                          else
+                          {
+                              Summ+=bigNumbers[temp/10].length();
+                          }
+                        }
+                    }
+                }               
             }
+            System.out.println((Summ));
+            System.out.print("");
 	}
 
 }
